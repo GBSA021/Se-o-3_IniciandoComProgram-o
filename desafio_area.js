a910bcc0abd41f84d5a13a21b05fa91caf0f101c
Gabriel Almeida <100151324+GBSA021@users.noreply.github.com>
2022-05-30 15:08:57 -0300

    Add files via upload

---
+++ desafio_area.js
@@ -0,0 +1,9 @@
+// PI = 3.141592
+// PI * raio * raio
+
+const PI = 3.147592;
+let raio = 10;
+let areaCirc = Math.PI * raio * raio;
+
+console.log("O valor da área é " + areaCirc + "m2.");  
+console.log(Math.PI);
